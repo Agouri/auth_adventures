086d7464e275db76046942ddda2b79a4859d16bf
StefanosDragoutsis <stevenpolitoed@gmail.com>
2016-08-04 20:49:21 +0300

    malakies
    poso_kakos?

---
+++ testing_some_sprite_shit.py
@@ -0,0 +1,141 @@
+import pygame
+import time
+from threading import Thread
+pygame.init()
+
+
+    
+
+        
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+
+pygame.display.set_caption("Edw ston agwna")
+
+white = (255,255,255)
+black = (0,0,0)
+
+hero = pygame.image.load("scyther.png")
+enemy = pygame.image.load("beedrill.png")
+heroRect = hero.get_rect()
+FPS = 15
+clock = pygame.time.Clock()
+
+def message_to_screen(msg, color):
+    textSurf, textRect = text_objects(msg,color)
+    text.Rect.center = (display_width / 2), (display_height / 2)
+    gameDisplay.blit(textSurf, textRect)
+
+def enemyMove():
+    location_x = 0
+    location_y = 100
+    #gameDisplay.blit(enemy,location_x, location_y)
+    while True:
+
+        gameDisplay.fill(white,[location_x - 10,location_y,50,50])
+        gameDisplay.blit(enemy,[location_x,location_y])
+        pygame.display.update()
+        location_x += 10
+        location_y += 0
+        clock.tick(10)
+
+##def blitHero():
+##    heroRect.center = (display_width/2), 550
+##    location_x = display_width/2
+##    location_y = 550
+##    gameDisplay.blit(hero,heroRect)
+
+##def moveEnemy(speed):
+##    location_x = 800
+##    location_y = 100
+##    while location_x <= 800 and location_x >=0:
+##        gameDisplay.blit(enemy,[location_x,location_y])
+##        location_x -= speed * 2
+##        location_y += speed
+    
+
+##def moveHero(location_x,location_y):
+##    if (location_x + 50 < 800 and location_x + 50 > 0) or (location_y + 50 < 600 and location_y + 50 > 0):
+##        gameDisplay.blit(hero,[location_x,location_y])
+##   
+       
+    
+def gameLoop():
+
+    gameRunning = True
+    gameOver = False
+
+
+    block_size = 10
+    location_x = 0
+    location_y = 0
+    X = 400
+    Y = 200
+    speed = 5
+    gameDisplay.fill(white)
+    
+
+    while gameRunning:
+
+        while gameOver:
+
+            gameDisplay.fill(white)
+            message_to_screen("Game Over Press C To Play Again Or Q to Quit")
+            pygame.display.update()
+            
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        gameOver = False
+                        gameRunning = False
+                    elif event.key == pygame.K_c:
+                        gameLoop()
+                elif event.type == pygame.QUIT:
+                    gameOver = False
+                    gameRunning = False
+       
+        for event in pygame.event.get():
+            
+            
+            if event.type == pygame.QUIT:
+                gameRunning = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if location_x - 50 >= 0:
+                         location_x -= 50
+                        
+                elif event.key == pygame.K_RIGHT:
+                    if location_x + 50 < 800:
+                           location_x += 50
+                    
+                elif event.key == pygame.K_UP:
+                     if location_y - 50 >= 0:
+                            location_y -= 50
+                    
+                elif event.key == pygame.K_DOWN:
+                     if location_y + 50 < 600:
+                          location_y += 50
+                    
+            gameDisplay.fill(white)
+            gameDisplay.blit(hero,[location_x,location_y])
+            pygame.display.update()
+            
+        
+        
+        clock.tick(10)
+
+    pygame.quit()
+    quit()
+beedrill1 = Thread(target = enemyMove)
+beedrill1.daemon = True
+beedrill1.start()
+
+gameLoop()
+    
+            
+
+            
+            
+            
